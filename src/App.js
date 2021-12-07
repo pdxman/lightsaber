4c86f6057efa4250ab3bc45b7ea6fab220136470
Crawford Vining <crawpdx@gmail.com>
2021-12-07 13:36:33 -0800

    update readme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+
 import './App.css';
+import Lightsabers from './Lightsabers'
+
 
 function App() {
+  const [collection, setCollection] = useState(Lightsabers)
+  const bgRed = { backgroundColor: 'red' }
+  const bgGreen = { backgroundColor: 'green' }
+
+  //console.log(lightsabers)
+
+  function toggle(){
+    setCollection(prevState => [
+      ({...prevState,
+        on: !prevState.on})
+      ]
+    )
+  }
+  
+  const showCollection = collection.map(saber => (
+    <div className="sabers" key={saber.id}>
+      <p>{saber.name}</p>
+      <p>{saber.color}</p>
+      <p style={saber.on ? bgRed : bgGreen } onClick={toggle}>{saber.on ? 'WATCH OUT' : 'ITS FINE'}</p>
+    </div>
+  ))
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Lightsabers!</h1>
+      {showCollection}
     </div>
   );
 }
---
+++ src/Lightsabers.js
@@ -0,0 +1,22 @@
+const lightsabers = [
+    {
+        name:'Luke Skywalker',
+        color:'green',
+        on: false,
+        id: 1
+    },
+    {
+        name:'Darth Vader',
+        color:'red',
+        on: true,
+        id: 2
+    },
+    {
+        name:'Obi-wan Kenobi',
+        color:'blue',
+        on: false,
+        id: 3
+    }
+]
+
+export default lightsabers
